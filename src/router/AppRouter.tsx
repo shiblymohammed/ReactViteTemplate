53f58d48e2d73ddd93b3a875d990c7a1aa1c7cdd
shibilymohammed <shibilymohammed75@gmail.com>
2025-08-17 19:40:15 +0530

    first commit

---
+++ src/router/AppRouter.tsx
@@ -0,0 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Layout from '../layout/Layout';
+import Home from '../pages/Home';
+
+
+const AppRouter = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default AppRouter; 
